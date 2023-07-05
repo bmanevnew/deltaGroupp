42392b350b3855680f46db48afccca2f4fb4c1af
Boris Manev <boris.manev.a44@learn.telerikacademy.com>
2023-07-05 14:53:44 +0300

    commit

--- src/main/java/com/example/newproject/controllers/mvc/HomeMvcController.java
+++ src/main/java/com/example/newproject/controllers/mvc/HomeMvcController.java
@@ -1,13 +1,10 @@
 package com.example.newproject.controllers.mvc;
 
-import com.example.newproject.exceptions.EmailSenderException;
 import com.example.newproject.models.QuoteDto;
 import com.example.newproject.services.impl.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
